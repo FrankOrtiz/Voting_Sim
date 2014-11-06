8a6c28b78a93d696de7bc3150e0557b2e9257c1c
Frank Ortiz <ortiz_frank@hotmail.com>
2014-11-06 16:36:27 -0500

    Changed some text styling.

--- voting_create.rb
+++ voting_create.rb
@@ -12,13 +12,17 @@ def create(object)
 end
 
 def create_politician
-	new_politician = Politician.new(ask_formal("What is the the politician's name?"), ask_formal("What is the politician's party?"))
-	puts "You have created a new politician."
-	puts "#{new_politician.party} Politician: #{new_politician.name}."
+	new_politician = Politician.new(ask_formal("What is the the politician's name?"), ask_party("What is the politician's party?"))
+	unless new_politician.nil?
+		puts "You have created a new politician."
+		puts "#{new_politician.party} Politician: #{new_politician.name}."
+		break_line
+	end
 end
 
 def create_person
 	new_person = Person.new(ask_formal("What is the the person's name?"), ask_formal("What is the person's politics?"))
 	puts "You have created a new person."
 	puts "#{new_person.politics} Person: #{new_person.name}."
+	break_line
 end
--- voting_methods.rb
+++ voting_methods.rb
@@ -1,18 +1,39 @@
 require './voting_update.rb'
 require './voting_create.rb'
 
+def break_line(br=nil)
+	puts "------------------------------------------------------"
+	if br
+		puts ""
+	end
+end
+
 def ask(question)
+	break_line
 	puts question
+	break_line
+	break_line(true)
 	gets.chomp.downcase
 end
 
 def ask_formal(question)
+	break_line
 	puts question
+	break_line(true)
 	gets.chomp.split.map(&:capitalize).join(' ')
 end
 
+def ask_party(question)
+	party = ask_formal(question)
+	if Politician.parties.include? party
+		return party
+	else 
+		ask_party("	  Please choose a valid party.\n \t    #{Politician.parties[0]} or #{Politician.parties[1]} ")
+	end
+end
+
 def list
-	puts "---------Politicians---------"
+	puts "----------------------Politicians---------------------"
 	puts ""
 	if Politician.all.empty?
 		puts "*There are no politicians currently running"
@@ -20,8 +41,8 @@ def list
 		Politician.list
 	end
 	puts ""
-	puts "-----------------------------"
-	puts "-----------Person------------"
+	break_line
+	puts "------------------------Person------------------------"
 		puts ""
 	if Person.all.empty?
 		puts "*There are no people currently voting"
@@ -29,5 +50,5 @@ def list
 		Person.list
 	end
 	puts ""
-	puts "-----------------------------"
+	break_line
 end
